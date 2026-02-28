823d6e8ef138fa40a0f8974489a6f0ee7e3668fa
Dunrab <72047814+Dunrab@users.noreply.github.com>
2026-02-27 23:57:11 -0500

    this is a bandaide fix for whitelists to be enabled (#139)

--- Content.Server/Connection/ConnectionManager.cs
+++ Content.Server/Connection/ConnectionManager.cs
@@ -312,8 +312,8 @@ namespace Content.Server.Connection
                 return (ConnectionDenyReason.Full, Loc.GetString("soft-player-cap-full"), null);
             }
 
-            // DeltaV - Replace existing softwhitelist implementation
-            if (false)//if (_cfg.GetCVar(CCVars.WhitelistEnabled) && adminData is null)
+            // Floofstation FUCKING WHITELISTS
+            if (_cfg.GetCVar(CCVars.WhitelistEnabled) && adminData is null)
             {
                 if (_whitelists is null)
                 {
@@ -344,26 +344,26 @@ namespace Content.Server.Connection
 
             // DeltaV - Soft whitelist improvements
             // TODO: replace this with a whitelist config prototype with a connected whitelisted players condition
-            if (_cfg.GetCVar(CCVars.WhitelistEnabled))
-            {
-                var connectedPlayers = _plyMgr.PlayerCount;
-                var connectedWhitelist = _connectedWhitelistedPlayers.Count;
+            //if (_cfg.GetCVar(CCVars.WhitelistEnabled))
+            //{
+               // var connectedPlayers = _plyMgr.PlayerCount;
+                //var connectedWhitelist = _connectedWhitelistedPlayers.Count;
 
-                var slots = 25;
+                //var slots = 25;
 
-                var noSlotsOpen = slots > 0 && slots < connectedPlayers - connectedWhitelist;
+                //var noSlotsOpen = slots > 0 && slots < connectedPlayers - connectedWhitelist;
 
-                if (noSlotsOpen && await _db.GetWhitelistStatusAsync(userId) == false
-                                     && adminData is null)
-                {
-                    var msg = Loc.GetString("whitelist-not-whitelisted-peri");
+                //if (noSlotsOpen && await _db.GetWhitelistStatusAsync(userId) == false
+                //                     && adminData is null)
+                //{
+                //    var msg = Loc.GetString("whitelist-not-whitelisted-peri");
 
-                    if (slots > 0)
-                        msg += "\n" + Loc.GetString("whitelist-playercount-invalid", ("min", slots), ("max", _cfg.GetCVar(CCVars.SoftMaxPlayers)));
+                //    if (slots > 0)
+                //        msg += "\n" + Loc.GetString("whitelist-playercount-invalid", ("min", slots), ("max", _cfg.GetCVar(CCVars.SoftMaxPlayers)));
 
-                    return (ConnectionDenyReason.Whitelist, msg, null);
-                }
-            }
+                //    return (ConnectionDenyReason.Whitelist, msg, null);
+                //}
+            //}
 
             // ALWAYS keep this at the end, to preserve the API limit.
             if (_cfg.GetCVar(CCVars.GameIPIntelEnabled) && adminData == null)
